7f410d3ff1ef5fb9b1b26fada365b4d71382e7a6
Oleg <bezrukov-oa@nm.ru>
2021-09-19 22:40:48 +0300

    add tags to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -48,6 +48,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :summary, :body, :image)
+    params.require(:post).permit(:title, :summary, :body, :image, :all_tags)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_19_170019) do
+ActiveRecord::Schema.define(version: 2021_09_19_182949) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -21,4 +21,20 @@ ActiveRecord::Schema.define(version: 2021_09_19_170019) do
     t.string "image"
   end
 
+  create_table "taggings", force: :cascade do |t|
+    t.integer "post_id", null: false
+    t.integer "tag_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_taggings_on_post_id"
+    t.index ["tag_id"], name: "index_taggings_on_tag_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "taggings", "posts"
 end
