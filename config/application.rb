0e4fc20622e648d330fa69158adc13d40f101cb3
Oleg <bezrukov-oa@nm.ru>
2021-09-19 19:46:32 +0300

    add localization

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Blog
   class Application < Rails::Application
+    config.i18n.default_locale = :ru
+    config.time_zone = 'Moscow'
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
